871af062a6acad8c5ba67f2e55acda0dd8e9a919
J.Tadebois <jpm.tadebois@gmail.com>
2020-12-31 17:31:40 +0400

    Add DELETE method on path /api/meetings/ to delete all meetings.

--- server/meetings-router.js
+++ server/meetings-router.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const {getAllFromDatabase, createMeeting, addToDatabase} = require('./db.js');
+const {
+  getAllFromDatabase,
+  createMeeting,
+  addToDatabase,
+  deleteAllFromDatabase,
+} = require('./db.js');
 const meetingsRouter = new express.Router();
 
 meetingsRouter.get('/', (req, res) => {
@@ -10,6 +15,15 @@ meetingsRouter.get('/', (req, res) => {
 meetingsRouter.post('/', (req, res) => {
   const newMeeting = addToDatabase('meetings', createMeeting());
   res.send(newMeeting);
+});
+
+meetingsRouter.delete('/', (req, res) => {
+  const meetings = deleteAllFromDatabase('meetings');
+  if (meetings) {
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
 });
 
 module.exports = meetingsRouter;
