f6f0f12b880b36f0aaff189a4650702b37b9949c
J.Tadebois <jpm.tadebois@gmail.com>
2020-12-31 17:28:51 +0400

    Add POST method on path /api/meetings to create a new meeting.

--- server/meetings-router.js
+++ server/meetings-router.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const {getAllFromDatabase} = require('./db.js');
+const {getAllFromDatabase, createMeeting, addToDatabase} = require('./db.js');
 const meetingsRouter = new express.Router();
 
 meetingsRouter.get('/', (req, res) => {
   const meetings = getAllFromDatabase('meetings');
   res.send(meetings);
+});
+
+meetingsRouter.post('/', (req, res) => {
+  const newMeeting = addToDatabase('meetings', createMeeting());
+  res.send(newMeeting);
 });
 
 module.exports = meetingsRouter;
