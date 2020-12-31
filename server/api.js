3e7b22ac2832e03b306562128cd2e3aa80878ef3
J.Tadebois <jpm.tadebois@gmail.com>
2020-12-31 17:22:46 +0400

    Add meetings router to api router.

--- server/api.js
+++ server/api.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const minionsRouter = require('./minions-router.js');
 const ideasRouter = require('./ideas-router.js');
+const meetingsRouter = require('./meetings-router.js');
 
 const apiRouter = new express.Router();
 
 apiRouter.use('/minions', minionsRouter);
 apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', meetingsRouter);
 
 module.exports = apiRouter;
---
+++ server/meetings-router.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const meetingsRouter = new express.Router();
+
+
+module.exports = meetingsRouter;
