6f8552778dddfebdba5516f39114c25ff21ed50a
J.Tadebois <jpm.tadebois@gmail.com>
2020-12-31 20:04:19 +0400

    Add PUT method on path /api/minions/:minionId/work/:workId
    The method updates a work object with passed in workId for a minion with id minionId.

--- server/work-router.js
+++ server/work-router.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const {getAllWorkByMinionId, addToDatabase} = require('./db.js');
+const {
+  getAllWorkByMinionId,
+  addToDatabase,
+  updateInstanceInDatabase,
+} = require('./db.js');
 const workRouter = new express.Router({mergeParams: true});
 
 workRouter.get('/', (req, res) => {
@@ -30,6 +34,29 @@ workRouter.post('/', validateWork, (req, res) => {
         minionId: req.minion.id,
       });
   res.send(newWork);
+});
+
+workRouter.param('workId', (req, res, next, workId) => {
+  const allWorkForMinion = getAllWorkByMinionId(req.minion.id);
+  const work = allWorkForMinion.filter((element) => element.id === workId);
+  if (work) {
+    req.work = work[0];
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
+workRouter.put('/:workId', validateWork, (req, res) => {
+  req.work.title = req.title;
+  req.work.description = req.description;
+  req.work.hours = req.hours;
+  const work = updateInstanceInDatabase('work', req.work);
+  if (work) {
+    res.send(work);
+  } else {
+    res.status(404).send();
+  }
 });
 
 module.exports = workRouter;
