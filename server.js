c244cb88d1efa85eaf3b0b5309fbcea42d458fbc
J.Tadebois <jpm.tadebois@gmail.com>
2020-12-31 04:47:10 +0400

    Add router mounted at path /api to the express app

--- server.js
+++ server.js
@@ -14,9 +14,8 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-// Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) {
