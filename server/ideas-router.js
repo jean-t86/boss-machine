757f9578d5b122e370592869efa6fa22f6a5435c
J.Tadebois <jpm.tadebois@gmail.com>
2020-12-31 16:48:33 +0400

    Add POST method on path /api/ideas to create a new idea.

--- server/ideas-router.js
+++ server/ideas-router.js
@@ -1,10 +1,37 @@
 const express = require('express');
-const {getAllFromDatabase} = require('./db.js');
+const {getAllFromDatabase, addToDatabase} = require('./db.js');
 const ideasRouter = new express.Router();
 
 ideasRouter.get('/', (req, res) => {
   const ideas = getAllFromDatabase('ideas');
   res.send(ideas);
+});
+
+const validateIdea = (req, res, next) => {
+  const name = req.query.name;
+  const description = req.query.description;
+  const numWeeks = Number(req.query.numWeeks);
+  const weeklyRevenue = Number(req.query.weeklyRevenue);
+
+  if (name && description && numWeeks && weeklyRevenue) {
+    req.name = name;
+    req.description = description;
+    req.numWeeks = numWeeks;
+    req.weeklyRevenue = weeklyRevenue;
+    next();
+  } else {
+    res.status(400).send();
+  }
+};
+
+ideasRouter.post('/', validateIdea, (req, res) => {
+  const idea = addToDatabase('ideas', {
+    name: req.name,
+    description: req.description,
+    numWeeks: req.numWeeks,
+    weeklyRevenue: req.weeklyRevenue,
+  });
+  res.send(idea);
 });
 
 module.exports = ideasRouter;
