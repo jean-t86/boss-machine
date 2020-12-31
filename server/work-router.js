8737ecf0282aca2652b9151967db888854f861b4
J.Tadebois <jpm.tadebois@gmail.com>
2020-12-31 19:31:05 +0400

    Add GET method on path /api/minions/:minionId/work
    The method returns all work for a specific minion.

--- server/work-router.js
+++ server/work-router.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const {getAllWorkByMinionId} = require('./db.js');
 const workRouter = new express.Router({mergeParams: true});
 
+workRouter.get('/', (req, res) => {
+  const allWorkForMinion = getAllWorkByMinionId(req.minion.id);
+  res.send(allWorkForMinion);
+});
 
 module.exports = workRouter;
