fc20ecf0029c99e9e2a0375ab6ee37a2b6ab4ee9
J.Tadebois <jpm.tadebois@gmail.com>
2020-12-31 17:25:12 +0400

    Add GET method on path /api/meetings to get an array of all meetings.

--- server/meetings-router.js
+++ server/meetings-router.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const {getAllFromDatabase} = require('./db.js');
 const meetingsRouter = new express.Router();
 
+meetingsRouter.get('/', (req, res) => {
+  const meetings = getAllFromDatabase('meetings');
+  res.send(meetings);
+});
 
 module.exports = meetingsRouter;
