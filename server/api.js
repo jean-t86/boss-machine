59fdf52e69c96153ffb5080822ee3bc4b560d160
J.Tadebois <jpm.tadebois@gmail.com>
2020-12-31 15:33:30 +0400

    Add POST method on path /api/minions to create a new minion.

--- server/api.js
+++ server/api.js
@@ -1,10 +1,28 @@
 const express = require('express');
-const {getAllFromDatabase} = require('./db.js');
+const {getAllFromDatabase, addToDatabase} = require('./db.js');
 const apiRouter = new express.Router();
 
 apiRouter.get('/minions', (req, res) => {
   const minions = getAllFromDatabase('minions');
   res.send(minions);
+});
+
+const validateMinion = (req, res, next) => {
+  const name = req.query.name;
+  const title = req.query.title;
+  const salary = req.query.salary;
+
+  if (name && title && salary) {
+    req.minion = {name, title, salary};
+    next();
+  } else {
+    res.status(400).send();
+  }
+};
+
+apiRouter.post('/minions', validateMinion, (req, res) => {
+  const minion = addToDatabase('minions', req.minion);
+  res.send(minion);
 });
 
 module.exports = apiRouter;
