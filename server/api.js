ad5519f12db5397c5d6ab56f65874bbd968841b2
J.Tadebois <jpm.tadebois@gmail.com>
2021-01-04 17:15:25 +0400

    Refactor apiRouter to use .json instead of .send

--- server/api.js
+++ server/api.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const {getAllFromDatabase, getFromDatabaseById} = require('./db.js');
+const {
+  getAllFromDatabase,
+  getFromDatabaseById,
+} = require('./db.js');
 const apiRouter = new express.Router();
 
 apiRouter.get('/minions', (req, res) => {
   const minions = getAllFromDatabase('minions');
-  res.status(200).send({minions});
+  res.status(200).json({minions});
 });
 
 apiRouter.get('/minions/:minionId', (req, res) => {
   const id = req.params.minionId;
   const minion = getFromDatabaseById('minions', id);
   if (minion) {
-    res.status(200).send(minion);
+    res.status(200).json(minion);
+  } else {
+    res.status(404).json();
+  }
+});
+
   } else {
     res.status(404).send();
   }
