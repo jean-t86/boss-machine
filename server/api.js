f05779f53e73b4bfa2f47706fc5a3c0e1a4e115c
J.Tadebois <jpm.tadebois@gmail.com>
2021-01-04 11:31:39 +0400

    Add GET minion by id

--- server/api.js
+++ server/api.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const {getAllFromDatabase} = require('./db.js');
+const {getAllFromDatabase, getFromDatabaseById} = require('./db.js');
 const apiRouter = new express.Router();
 
 apiRouter.get('/minions', (req, res) => {
   const minions = getAllFromDatabase('minions');
-  res.status(200).send(minions);
+  res.status(200).send({minions});
+});
+
+apiRouter.get('/minions/:minionId', (req, res) => {
+  const id = req.params.minionId;
+  const minion = getFromDatabaseById('minions', id);
+  if (minion) {
+    res.status(200).send(minion);
+  } else {
+    res.status(404).send();
+  }
 });
 
 module.exports = apiRouter;
--- test/api-test.js
+++ test/api-test.js
@@ -40,4 +40,50 @@ describe('Boss Machine API', function() {
           });
     });
   });
+
+  describe('GET a minion by id', function() {
+    it('returns status code 200', function(done) {
+      request(server.expressApp)
+          .get('/api/minions/1')
+          .expect(200, done);
+    });
+
+    it('returns the minion with id minionId', function() {
+      request(server.expressApp)
+          .get('/api/minions/1')
+          .expect(200)
+          .then((res) => {
+            assert.ok(res.body.minion);
+            const minion = res.body;
+            expect(minion).to.have.ownProperty('id');
+            expect(minion).to.have.ownProperty('name');
+            expect(minion).to.have.ownProperty('title');
+            expect(minion).to.have.ownProperty('salary');
+            expect(minion).to.have.ownProperty('weaknesses');
+          });
+
+      it('returned minion has the correct id', function() {
+        const id = '4';
+        request(server.expressApp)
+            .get(`/api/minions/${id}`)
+            .expect(200)
+            .then((res) => {
+              const minion = res.body;
+              assert.strictEqual(minion.id, id);
+            });
+      });
+
+      it('returns 404 if minion with id cannot be found', function() {
+        request(server.expressApp)
+            .get('/api/minions/35')
+            .expect(404);
+      });
+
+      it('resturns 404 if the id is a string', function(done) {
+        request(server.expressApp)
+            .get('/api/minion/adei')
+            .expect(404, done);
+      });
+    });
+  });
 });
